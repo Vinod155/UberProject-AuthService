a979e31ea38eadbdf6a3c8d460cca56d853a79b8
Vinod Kumar <vkumar01@landolakes.com>
2024-08-02 11:46:20 +0530

    Added User details class

---
+++ src/main/java/com/example/UberProject_AuthService/helpers/AuthPassengerDetails.java
@@ -0,0 +1,53 @@
+package com.example.UberProject_AuthService.helpers;
+
+import com.example.UberProject_AuthService.models.Passenger;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AuthPassengerDetails extends Passenger implements UserDetails {
+
+    private String username;
+
+    private String password;
+
+    public AuthPassengerDetails(Passenger passenger)
+    {
+        this.username=passenger.getEmail();
+        this.password=passenger.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    //below set of methods doesn't bother us
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
